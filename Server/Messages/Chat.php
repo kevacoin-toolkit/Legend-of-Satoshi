03ed90cf4710a44a6087912906e76187ed4ab73e
kawww <39939137+kawww@users.noreply.github.com>
2021-03-23 19:27:42 +0800

    Add files via upload

--- Server/Messages/Chat.php
+++ Server/Messages/Chat.php
@@ -15,12 +15,115 @@ namespace Server\Messages;
 
 use Server\Utils;
 
+
+
 class Chat
 {
     public $playerId = 0;
     public $message = null;
     public function __construct($player, $message)
     {
+		//emoji
+
+		if($message=="/0"){$message="<img src=img/emoji/0.gif width=27>";}
+
+		//check
+
+		$kname=$player->name;
+
+				
+		$commtool=explode('|', $kname);
+		
+
+		if($message=="/check")
+			{
+
+					$kname=$player->name;
+
+				
+					$commtool=explode('|', $kname);
+
+					
+					if(!$commtool[2]){$message="<font color=red>RAVENCOIN</font>";}else{$message="<font color=chartreuse>RAVENCOIN</font>";}	
+					if(!$commtool[3]){$message=$message." <font color=red>DOGECOIN </font>";}else{$message=$message." <font color=chartreuse>DOGECOIN</font>";}	
+					if(!$commtool[4]){$message=$message." <font color=red>BITCOIN</font>";}else{$message=$message." <font color=chartreuse>BITCOIN</font>";}
+
+					$message=$message." <a href=https://keva.app/?5322812 target=_blank>[ + ]</a>";
+			
+			}
+
+		//balance
+
+		$rpc = new Raven();
+		$kpc = new Keva();
+		$dpc = new Doge();
+		
+
+		if($message=="/kva" or $message=="/keva")
+			{
+			$message=$kpc->getbalance("");
+			$message=$message." KVA in the world";
+			}
+
+		if($message=="/rvn")
+			{
+			$message=$rpc->getbalance("");
+			$message=$message." RVN in the world";
+			}
+
+		if($message=="/doge")
+			{
+			$message=$dpc->getbalance("");
+			$message=$message." DOGE in the world";
+			}
+		
+		/*if($message=="/btc")
+			{
+			$message=$bpc->getbalance("");
+			$message=$message." BTC in the world";
+			}
+		*/
+
+
+		//coin
+			
+			if($message=="/coin")
+			{
+
+			if($commtool[1] !=""){$message="<a target=\"_blank\"  href=https://explorer.kevacoin.org/address/".$commtool[1].">[KVA]</a>";}
+
+			if($commtool[2] !=""){$message=$message." <a target=\"_blank\"  href=https://ravencoin.network/address/".$commtool[2].">[RVN]</a>";}
+
+			if($commtool[3] !=""){$message=$message." <a target=\"_blank\"  href=https://blockchair.com/dogecoin/address/".$commtool[3].">[DOGE]</a>";}
+
+			if($commtool[4] !=""){$message=$message." <a target=\"_blank\"  href=https://blockchair.com/bitcoin/address/".$commtool[4].">[BTC]</a>";}
+
+			}
+
+
+
+		//space
+			
+			if($message=="/space")
+			{
+			$getnum=explode('*', $commtool[0]);
+
+			$message="<a target=\"_blank\"  href=https://keva.app/?".$getnum[0].">keva.app/?".$getnum[0]."</a>";
+
+
+			}
+
+	   //nft
+			
+			if($message=="/nft")
+			{
+
+			if($commtool[2] !=""){$message="<a target=\"_blank\"   href=http://galaxyos.io/?lang=&asset=".$commtool[2].">[RVN]</a>";}
+
+
+			}
+
+			
         $this->playerId = $player->id;
         $this->message = $message ;
     }
@@ -34,3 +137,348 @@ class Chat
     }
 }
 
+
+class Raven {
+
+    private $proto;
+
+    private $url;
+    private $CACertificate;
+
+    public $status;
+    public $error;
+    public $raw_response;
+    public $response;
+
+    private $id = 0;
+
+    public function __construct($url = null) {
+		
+        $this->username      = 'galaxy'; // RPC Username
+        $this->password      = 'frontier'; // RPC Password
+        //$this->host          = '192.168.152.6'; // Localhost
+		$this->host          = '127.0.0.1'; // Localhost
+        $this->port          = '9991';
+        $this->url           = $url;
+
+        $this->proto         = 'http';
+        $this->CACertificate = null;
+    }
+
+    public function setSSL($certificate = null) {
+        $this->proto         = 'https';
+        $this->CACertificate = $certificate;
+    }
+
+    public function __call($method, $params) {
+        $this->status       = null;
+        $this->error        = null;
+        $this->raw_response = null;
+        $this->response     = null;
+
+        $params = array_values($params);
+
+        $this->id++;
+
+        $request = json_encode(array(
+            'method' => $method,
+            'params' => $params,
+            'id'     => $this->id
+        ));
+
+        $curl    = curl_init("{$this->proto}://{$this->host}:{$this->port}/{$this->url}");
+        $options = array(
+            CURLOPT_HTTPAUTH       => CURLAUTH_BASIC,
+            CURLOPT_USERPWD        => $this->username . ':' . $this->password,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_HTTPHEADER     => array('Content-type: text/plain'),
+            CURLOPT_POST           => true,
+            CURLOPT_POSTFIELDS     => $request
+        );
+
+        if (ini_get('open_basedir')) {
+            unset($options[CURLOPT_FOLLOWLOCATION]);
+        }
+
+        if ($this->proto == 'https') {
+            if (!empty($this->CACertificate)) {
+                $options[CURLOPT_CAINFO] = $this->CACertificate;
+                $options[CURLOPT_CAPATH] = DIRNAME($this->CACertificate);
+            } else {
+                $options[CURLOPT_SSL_VERIFYPEER] = false;
+            }
+        }
+
+        curl_setopt_array($curl, $options);
+
+        $this->raw_response = curl_exec($curl);
+        $this->response     = json_decode($this->raw_response, true);
+
+        $this->status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $curl_error = curl_error($curl);
+
+        curl_close($curl);
+
+        if (!empty($curl_error)) {
+            $this->error = $curl_error;
+        }
+
+        if ($this->response['error']) {
+            $this->error = $this->response['error']['message'];
+        } elseif ($this->status != 200) {
+            switch ($this->status) {
+                case 400:
+                    $this->error = 'HTTP_BAD_REQUEST';
+                    break;
+                case 401:
+                    $this->error = 'HTTP_UNAUTHORIZED';
+                    break;
+                case 403:
+                    $this->error = 'HTTP_FORBIDDEN';
+                    break;
+                case 404:
+                    $this->error = 'HTTP_NOT_FOUND';
+                    break;
+            }
+        }
+
+        if ($this->error) {
+			return false;
+        }
+
+        return $this->response['result'];
+    }
+}
+
+class Keva {
+
+    private $proto;
+
+    private $url;
+    private $CACertificate;
+
+    public $status;
+    public $error;
+    public $raw_response;
+    public $response;
+
+    private $id = 0;
+
+    public function __construct($url = null) {
+		
+        $this->username      = 'galaxy'; // RPC Username
+        $this->password      = 'frontier'; // RPC Password
+        //$this->host          = '192.168.152.6'; // Localhost
+		$this->host          = '127.0.0.1'; // Localhost
+        $this->port          = '9992';
+        $this->url           = $url;
+
+        $this->proto         = 'http';
+        $this->CACertificate = null;
+    }
+
+    public function setSSL($certificate = null) {
+        $this->proto         = 'https';
+        $this->CACertificate = $certificate;
+    }
+
+    public function __call($method, $params) {
+        $this->status       = null;
+        $this->error        = null;
+        $this->raw_response = null;
+        $this->response     = null;
+
+        $params = array_values($params);
+
+        $this->id++;
+
+        $request = json_encode(array(
+            'method' => $method,
+            'params' => $params,
+            'id'     => $this->id
+        ));
+
+        $curl    = curl_init("{$this->proto}://{$this->host}:{$this->port}/{$this->url}");
+        $options = array(
+            CURLOPT_HTTPAUTH       => CURLAUTH_BASIC,
+            CURLOPT_USERPWD        => $this->username . ':' . $this->password,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_HTTPHEADER     => array('Content-type: text/plain'),
+            CURLOPT_POST           => true,
+            CURLOPT_POSTFIELDS     => $request
+        );
+
+        if (ini_get('open_basedir')) {
+            unset($options[CURLOPT_FOLLOWLOCATION]);
+        }
+
+        if ($this->proto == 'https') {
+            if (!empty($this->CACertificate)) {
+                $options[CURLOPT_CAINFO] = $this->CACertificate;
+                $options[CURLOPT_CAPATH] = DIRNAME($this->CACertificate);
+            } else {
+                $options[CURLOPT_SSL_VERIFYPEER] = false;
+            }
+        }
+
+        curl_setopt_array($curl, $options);
+
+        $this->raw_response = curl_exec($curl);
+        $this->response     = json_decode($this->raw_response, true);
+
+        $this->status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $curl_error = curl_error($curl);
+
+        curl_close($curl);
+
+        if (!empty($curl_error)) {
+            $this->error = $curl_error;
+        }
+
+        if ($this->response['error']) {
+            $this->error = $this->response['error']['message'];
+        } elseif ($this->status != 200) {
+            switch ($this->status) {
+                case 400:
+                    $this->error = 'HTTP_BAD_REQUEST';
+                    break;
+                case 401:
+                    $this->error = 'HTTP_UNAUTHORIZED';
+                    break;
+                case 403:
+                    $this->error = 'HTTP_FORBIDDEN';
+                    break;
+                case 404:
+                    $this->error = 'HTTP_NOT_FOUND';
+                    break;
+            }
+        }
+
+        if ($this->error) {
+			return false;
+        }
+
+        return $this->response['result'];
+    }
+}
+
+class Doge {
+
+    private $proto;
+
+    private $url;
+    private $CACertificate;
+
+    public $status;
+    public $error;
+    public $raw_response;
+    public $response;
+
+    private $id = 0;
+
+    public function __construct($url = null) {
+		
+        $this->username      = 'galaxy'; // RPC Username
+        $this->password      = 'frontier'; // RPC Password
+        //$this->host          = '192.168.152.6'; // Localhost
+		$this->host          = '127.0.0.1'; // Localhost
+        $this->port          = '9993';
+        $this->url           = $url;
+
+        $this->proto         = 'http';
+        $this->CACertificate = null;
+    }
+
+    public function setSSL($certificate = null) {
+        $this->proto         = 'https';
+        $this->CACertificate = $certificate;
+    }
+
+    public function __call($method, $params) {
+        $this->status       = null;
+        $this->error        = null;
+        $this->raw_response = null;
+        $this->response     = null;
+
+        $params = array_values($params);
+
+        $this->id++;
+
+        $request = json_encode(array(
+            'method' => $method,
+            'params' => $params,
+            'id'     => $this->id
+        ));
+
+        $curl    = curl_init("{$this->proto}://{$this->host}:{$this->port}/{$this->url}");
+        $options = array(
+            CURLOPT_HTTPAUTH       => CURLAUTH_BASIC,
+            CURLOPT_USERPWD        => $this->username . ':' . $this->password,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_HTTPHEADER     => array('Content-type: text/plain'),
+            CURLOPT_POST           => true,
+            CURLOPT_POSTFIELDS     => $request
+        );
+
+        if (ini_get('open_basedir')) {
+            unset($options[CURLOPT_FOLLOWLOCATION]);
+        }
+
+        if ($this->proto == 'https') {
+            if (!empty($this->CACertificate)) {
+                $options[CURLOPT_CAINFO] = $this->CACertificate;
+                $options[CURLOPT_CAPATH] = DIRNAME($this->CACertificate);
+            } else {
+                $options[CURLOPT_SSL_VERIFYPEER] = false;
+            }
+        }
+
+        curl_setopt_array($curl, $options);
+
+        $this->raw_response = curl_exec($curl);
+        $this->response     = json_decode($this->raw_response, true);
+
+        $this->status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $curl_error = curl_error($curl);
+
+        curl_close($curl);
+
+        if (!empty($curl_error)) {
+            $this->error = $curl_error;
+        }
+
+        if ($this->response['error']) {
+            $this->error = $this->response['error']['message'];
+        } elseif ($this->status != 200) {
+            switch ($this->status) {
+                case 400:
+                    $this->error = 'HTTP_BAD_REQUEST';
+                    break;
+                case 401:
+                    $this->error = 'HTTP_UNAUTHORIZED';
+                    break;
+                case 403:
+                    $this->error = 'HTTP_FORBIDDEN';
+                    break;
+                case 404:
+                    $this->error = 'HTTP_NOT_FOUND';
+                    break;
+            }
+        }
+
+        if ($this->error) {
+			return false;
+        }
+
+        return $this->response['result'];
+    }
+}
